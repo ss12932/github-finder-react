a022b91acf3769ca60a344780ce654c6d1ae680d
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-08 13:42:49 +0100

    display users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
 
 function UserResults() {
@@ -25,7 +26,7 @@ function UserResults() {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
